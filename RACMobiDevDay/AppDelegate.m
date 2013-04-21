d00726d961135dc7d3b968cca475082e2aaf2b00
Andrew Sardone <andrew@andrewsardone.com>
2013-04-21 16:26:10 -0400

    Add blank Xcode project (via 'Empty' template)

---
+++ RACMobiDevDay/AppDelegate.m
@@ -0,0 +1,14 @@
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    // Override point for customization after application launch.
+    self.window.backgroundColor = [UIColor whiteColor];
+    [self.window makeKeyAndVisible];
+    return YES;
+}
+
+@end
